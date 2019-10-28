90f38e42b81e2e8d4021270332fdc671fbc33092
Bart <bartlomiej.jedruszak@pollub.edu.pl>
2019-10-28 19:54:24 +0100

    init

---
+++ src/main/java/com/bj/betapi/BetApiApplication.java
@@ -0,0 +1,13 @@
+package com.bj.betapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bj/betapi/BetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bj.betapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
