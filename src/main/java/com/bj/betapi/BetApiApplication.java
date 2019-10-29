641d18927ad6b807e0325a6051993e0804100a98
Bart <bartlomiej.jedruszak@pollub.edu.pl>
2019-10-29 12:43:16 +0100

    deploy test

--- src/main/java/com/bj/betapi/BetApiApplication.java
+++ src/main/java/com/bj/betapi/BetApiApplication.java
@@ -2,9 +2,17 @@ package com.bj.betapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BetApiApplication {
+
+    @GetMapping("/")
+    public String testMsg() {
+        return "Deployment works!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BetApiApplication.class, args);
